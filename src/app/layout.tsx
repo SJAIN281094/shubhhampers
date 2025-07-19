c6bff374bfb2939251bda3e4cb211a60e240b94f
Aman Jain <aman.jain2@go-mmt.com>
2025-07-19 22:24:07 +0530

    added google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -132,6 +132,12 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');`
+          }}
+        />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link
@@ -198,7 +204,19 @@ export default function RootLayout({
           <script async src='https://www.instagram.com/embed.js' />
         )}
       </head>
-      <body className='antialiased'>{children}</body>
+      <body className='antialiased'>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            title='Google Tag Manager'
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}`}
+            height='0'
+            width='0'
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
