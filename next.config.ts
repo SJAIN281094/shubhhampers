0f4cd2a1b2498657c009504eb652e43acfe75c0d
Shubham Jain <v-sjain4@expediagroup.com>
2025-07-23 22:14:22 +0530

    fix image loading

--- next.config.ts
+++ next.config.ts
@@ -13,10 +13,10 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 31536000,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    loader: "default",
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    loader: "default",
-    path: "/_next/image/",
+    // path: "/_next/image/",
     remotePatterns: [
       {
         protocol: "https",
