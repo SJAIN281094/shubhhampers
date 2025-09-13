88b5fd8672ed70486b733268b29222f1826dd031
Aman Jain <aman.jain2@go-mmt.com>
2025-09-13 15:20:14 +0530

    removed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,20 +145,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-7ZW7LLYWMF"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-7ZW7LLYWMF');
-          `}
-        </Script>
-
         {/* Critical Structured Data - Load immediately */}
         <script
           type="application/ld+json"
