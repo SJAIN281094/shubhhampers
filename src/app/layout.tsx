358a14fb5abf7b6debb41bc9afea9ffa0fbcf97e
Aman Jain <aman.jain2@go-mmt.com>
2025-09-13 15:04:45 +0530

    added changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,6 +145,20 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7ZW7LLYWMF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7ZW7LLYWMF');
+          `}
+        </Script>
+
         {/* Critical Structured Data - Load immediately */}
         <script
           type="application/ld+json"
